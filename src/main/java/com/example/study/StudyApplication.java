f5d3311c3c9fa79ddfe53495d431dc2ab6eb79ec
hcgo97 <hcgo_o@naver.com>
2020-12-01 11:00:00 +0900

    inflearn-restapi-study 2020.12.21

---
+++ src/main/java/com/example/study/StudyApplication.java
@@ -0,0 +1,29 @@
+package com.example.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class StudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyApplication.class, args);
+    }
+
+    //다국어처리
+    @Bean
+    public LocaleResolver localeResolver(){
+
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+
+        return localeResolver;
+
+    }
+
+}
